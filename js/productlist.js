9ddc4fb96bc065b5b852df97f9b1cd06ff5934e9
Mikkel <mist0009@stud.ek.dk>
2026-03-04 12:38:45 +0100

    productlistdone

---
+++ js/productlist.js
@@ -0,0 +1,35 @@
+const params = new URLSearchParams(window.location.search);
+const myCategory = params.get("category");
+
+const listContainer = document.querySelector(".produkter");
+
+const fetchUrl = myCategory
+  ? `https://kea-alt-del.dk/t7/api/products?category=${encodeURIComponent(myCategory)}`
+  : "https://kea-alt-del.dk/t7/api/products";
+
+function getProducts() {
+  fetch(fetchUrl)
+    .then((res) => res.json())
+    .then((products) => showProducts(products));
+}
+
+function showProducts(products) {
+  listContainer.innerHTML = "";
+
+  products.forEach((product) => {
+    listContainer.innerHTML += `
+     <div class="produkt">
+            <a href="product.html">
+                <img class="tshirt" src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="billede3">
+            </a>
+            <h3 class="h3produkter">${product.productdisplayname}</h3>
+            <p class="pprodukter">${product.articletype}</p>
+            <p class="pprodukter">${product.price}</p>
+            <a href="product.html?id=${product.id}" class="knap">Se mere</a>
+
+        </div>
+    `;
+  });
+}
+
+getProducts();
